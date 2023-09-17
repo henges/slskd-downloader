66d6cf10714472c0ecc6b90d27f1ecee3d1a7bd7
U-LAPTOP-4EP3DB2K\alexa <spiderland@outlook.com>
2023-09-17 11:02:49 +0800

    Init

---
+++ src/main/java/dev/polluxus/spotify_offline_playlist/SpotifyOfflinePlaylist.java
@@ -0,0 +1,9 @@
+package dev.polluxus.spotify_offline_playlist;
+
+public class SpotifyOfflinePlaylist {
+
+    // Open a playlist identifier
+    // Read the metadata from the playlist
+    // Use a music provider to download the files (caching)
+    // Organise the files in playlist order
+}
